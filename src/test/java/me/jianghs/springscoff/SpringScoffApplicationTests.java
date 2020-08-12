bec93eec8ff8aa70411153fd7c93d5f0f49ed2fe
jianghs430 <jianghs430@zto.com>
2020-08-12 16:56:22 +0800

    first commit

---
+++ src/test/java/me/jianghs/springscoff/SpringScoffApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jianghs.springscoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScoffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
