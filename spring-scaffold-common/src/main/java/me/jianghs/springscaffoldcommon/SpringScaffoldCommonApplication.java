b8e232748a08d40756348205610df368a9a52b90
jianghs430 <jianghs430@zto.com>
2020-08-14 14:52:51 +0800

    commit

---
+++ spring-scaffold-common/src/main/java/me/jianghs/springscaffoldcommon/SpringScaffoldCommonApplication.java
@@ -0,0 +1,13 @@
+package me.jianghs.springscaffoldcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScaffoldCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScaffoldCommonApplication.class, args);
+    }
+
+}
---
+++ spring-scaffold-common/src/test/java/me/jianghs/springscaffoldcommon/SpringScaffoldCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jianghs.springscaffoldcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScaffoldCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
