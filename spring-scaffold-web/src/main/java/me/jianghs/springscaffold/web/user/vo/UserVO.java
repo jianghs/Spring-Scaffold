8c49577693c9f6810a561fab0cdc1e02b59f8a3b
jianghs <jianghs_own@163.com>
2020-08-18 01:51:43 +0800

    change @notnull to @notblank

--- spring-scaffold-web/src/main/java/me/jianghs/springscaffold/web/user/vo/UserVO.java
+++ spring-scaffold-web/src/main/java/me/jianghs/springscaffold/web/user/vo/UserVO.java
@@ -20,18 +20,18 @@ public class UserVO implements Serializable {
     @NotBlank(message = "姓名不得为空")
     private String name;
 
-    @NotNull(message = "手机号不得为空")
+    @NotBlank(message = "手机号不得为空")
     @Length(min = 11, max = 11, message = "手机号必须11位")
     private String mobile;
 
-    @NotNull(message = "性别不得为空")
+    @NotBlank(message = "性别不得为空")
     @Pattern(regexp = "[FM]", message = "性别只能为M-男或者F-女")
     private String sex;
 
-    @NotNull(message = "邮箱不得为空")
+    @NotBlank(message = "邮箱不得为空")
     @Email(message = "邮箱格式不正确")
     private String email;
 
-    @NotNull(message = "出生日期不得为空")
+    @NotBlank(message = "出生日期不得为空")
     private String date;
 }
