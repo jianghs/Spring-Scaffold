810ca41df5786fb1937742f129bc662babcde945
jianghs430 <jianghs430@zto.com>
2020-08-17 13:59:15 +0800

    change structure

---
+++ spring-scaffold-repository/src/main/java/me/jianghs/springscaffold/repository/RepositoryApplication.java
@@ -0,0 +1,19 @@
+package me.jianghs.springscaffold.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className: RepositoryApplication
+ * @description: TODO
+ * @author: jianghs430
+ * @createDate: 2020/8/17 13:56
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class RepositoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoryApplication.class, args);
+    }
+
+}
